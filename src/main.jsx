4a06418fd702290ac8fef0c1076d63c46d30144b
sanehtth <sane.htth@gmail.com>
2025-12-21 14:02:09 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { ThemeProvider } from "./theme.jsx";
-import { AuthProvider } from "./AuthContext.jsx";
+import App from "./App";
+import { AuthProvider } from "./AuthContext";
+import { ThemeProvider } from "./theme";
 import "./theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
