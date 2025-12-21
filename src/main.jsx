b86a5c3e430b2067666bf970753c2956f11dd2ac
sanehtth <sane.htth@gmail.com>
2025-12-21 11:38:15 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { ThemeProvider } from "./theme.jsx";
 import { AuthProvider } from "./AuthContext.jsx";
 
