dabd780e598dead21fed16295d4ea8228a43cff6
sanehtth <sane.htth@gmail.com>
2025-12-21 11:47:39 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import { ThemeProvider } from "./theme.jsx";
 import { AuthProvider } from "./AuthContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
-  </React.StrictMode>
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+    <React.StrictMode>
+        <ThemeProvider>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </ThemeProvider>
+    </React.StrictMode>
 );
