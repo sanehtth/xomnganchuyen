1ea8ae789460433641f6e2dd6df9cab741ab5930
sanehtth <sane.htth@gmail.com>
2025-12-23 19:15:13 +0700

    Add files via upload

--- public/js/he-thong/auth.js
+++ public/js/he-thong/auth.js
@@ -9,10 +9,7 @@ import {
   onAuthStateChanged,
 } from "./firebase.js";
 
-import {
-  ensureUserDocument,
-  getUserDocument,
-} from "../data/userData.js";
+import { ensureUserDocument } from "../data/userData.js";
 
 // Trạng thái auth toàn cục để các module khác có thể dùng
 export const authState = {
@@ -23,11 +20,23 @@ export const authState = {
 
 // Đăng nhập với Google
 export async function loginWithGoogle() {
-  // Dung Redirect de tranh bi chan popup / mat "user gesture"
-  // (signInWithRedirect se chuyen trang sang Google)
-  await signInWithRedirect(auth, googleProvider);
-}
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    const firebaseUser = result.user;
 
+    // Đảm bảo có hồ sơ trong Firestore
+    const profile = await ensureUserDocument(firebaseUser);
+
+    authState.firebaseUser = firebaseUser;
+    authState.profile = profile;
+    authState.loading = false;
+
+    return { firebaseUser, profile };
+  } catch (err) {
+    console.error("Lỗi đăng nhập:", err);
+    throw err;
+  }
+}
 
 // Đăng xuất
 export async function logout() {
@@ -44,9 +53,7 @@ export async function logout() {
 
 // Lắng nghe thay đổi auth và load hồ sơ user
 export function subscribeAuthState(callback) {
-  
-  handleRedirectResultOnce();
-authState.loading = true;
+  authState.loading = true;
 
   return onAuthStateChanged(auth, async (firebaseUser) => {
     try {
@@ -61,10 +68,7 @@ authState.loading = true;
       authState.firebaseUser = firebaseUser;
 
       // Lấy hồ sơ từ Firestore (đã có ensureUserDocument ở lần login đầu)
-      let profile = await getUserDocument(firebaseUser.uid);
-      if (!profile) {
-        profile = await ensureUserDocument(firebaseUser);
-      }
+      let profile = await ensureUserDocument(firebaseUser);
 
       authState.profile = profile;
       authState.loading = false;
--- public/js/he-thong/firebase.js
+++ public/js/he-thong/firebase.js
@@ -1,7 +1,9 @@
-// public/js/he-thong/firebase.js
-// Firebase init (Auth + Firestore + Realtime Database)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
+// js/he-thong/firebase.js
+// Khoi tao Firebase + Firestore
+// Chi lam nhiem vu he thong, khong viet logic giao dien o day
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import {
   getAuth,
   GoogleAuthProvider,
@@ -9,7 +11,6 @@ import {
   signInWithPopup,
   signOut,
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
 import {
   getFirestore,
   doc,
@@ -21,56 +22,53 @@ import {
   query,
   orderBy,
   serverTimestamp,
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+  // realtime db
+  rtdb,
+  rtdbRef,
+  rtdbGet,
+  rtdbSet,
+  rtdbUpdate,
+  rtdbServerTimestamp,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import {
   getDatabase,
   ref as rtdbRef,
   get as rtdbGet,
   set as rtdbSet,
   update as rtdbUpdate,
-  onValue as rtdbOnValue,
-  runTransaction as rtdbRunTransaction,
   serverTimestamp as rtdbServerTimestamp,
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-// NOTE: Bạn chỉ cần đổi firebaseConfig khi đổi project Firebase.
+// Cau hinh Firebase
+// TODO: neu sau nay ban doi project Firebase thi chi can doi doan nay
 const firebaseConfig = {
   apiKey: "AIzaSyCsy8_u9ELGMiur-YyKsDYu1oU8YSpZKXY",
   authDomain: "xomnganchuyen.firebaseapp.com",
   projectId: "xomnganchuyen",
   storageBucket: "xomnganchuyen.firebasestorage.app",
   messagingSenderId: "335661705640",
-  appId: "1:335661705640:web:80de062fae1fcb3c99559a",
+  appId: "1:335661705640:web:8bde062fae1fcb3c99559d",
   measurementId: "G-21JSZ5G1EX",
-  // Quan trọng: chỉ rõ databaseURL để tránh cảnh báo region / host
-  databaseURL: "https://xomnganchuyen-default-rtdb.asia-southeast1.firebasedatabase.app",
 };
 
+// Khoi tao
 const app = initializeApp(firebaseConfig);
-
-// Auth
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
-
-// Firestore
 const db = getFirestore(app);
 
+
 // Realtime Database
-const rtdb = getDatabase(app, firebaseConfig.databaseURL);
-
+const rtdb = getDatabase(app);
+// Export nhung gi can cho cac file khac
 export {
-  // core
   app,
-
-  // auth
   auth,
   googleProvider,
   onAuthStateChanged,
   signInWithPopup,
   signOut,
-
-  // firestore
   db,
   doc,
   getDoc,
@@ -84,12 +82,9 @@ export {
 
   // realtime db
   rtdb,
-  getDatabase,
   rtdbRef,
   rtdbGet,
   rtdbSet,
   rtdbUpdate,
-  rtdbOnValue,
-  rtdbRunTransaction,
   rtdbServerTimestamp,
 };
