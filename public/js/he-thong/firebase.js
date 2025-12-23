31352763bf1c739399503ca70959d5a5682d3980
sanehtth <sane.htth@gmail.com>
2025-12-23 20:09:05 +0700

    Add files via upload

--- public/js/he-thong/firebase.js
+++ public/js/he-thong/firebase.js
@@ -1,16 +1,19 @@
-
 // js/he-thong/firebase.js
-// Khoi tao Firebase + Firestore
+// Khoi tao Firebase + Auth + Firestore + Realtime Database
 // Chi lam nhiem vu he thong, khong viet logic giao dien o day
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
 import {
   getAuth,
   GoogleAuthProvider,
   onAuthStateChanged,
   signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
   signOut,
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
 import {
   getFirestore,
   doc,
@@ -22,15 +25,8 @@ import {
   query,
   orderBy,
   serverTimestamp,
-
-  // realtime db
-  rtdb,
-  rtdbRef,
-  rtdbGet,
-  rtdbSet,
-  rtdbUpdate,
-  rtdbServerTimestamp,
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
 import {
   getDatabase,
   ref as rtdbRef,
@@ -38,37 +34,50 @@ import {
   set as rtdbSet,
   update as rtdbUpdate,
   serverTimestamp as rtdbServerTimestamp,
+  onValue as rtdbOnValue,
+  push as rtdbPush,
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 // Cau hinh Firebase
-// TODO: neu sau nay ban doi project Firebase thi chi can doi doan nay
 const firebaseConfig = {
   apiKey: "AIzaSyCsy8_u9ELGMiur-YyKsDYu1oU8YSpZKXY",
   authDomain: "xomnganchuyen.firebaseapp.com",
   projectId: "xomnganchuyen",
   storageBucket: "xomnganchuyen.firebasestorage.app",
   messagingSenderId: "335661705640",
-  appId: "1:335661705640:web:8bde062fae1fcb3c99559d",
-  measurementId: "G-21JSZ5G1EX",
+  appId: "1:335661705640:web:8bde062fae1fcb3c99559a",
+  measurementId: "G-2IJSZS61EX",
 };
 
 // Khoi tao
 const app = initializeApp(firebaseConfig);
+
+// Auth
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+
+// Firestore
 const db = getFirestore(app);
 
-
 // Realtime Database
+// Luu y: neu ban co RTDB URL rieng (multi-region), co the truyen them tham so thu 2 cho getDatabase(app, "<RTDB_URL>")
 const rtdb = getDatabase(app);
-// Export nhung gi can cho cac file khac
+
+// Export dung cho cac file khac
 export {
+  // core
   app,
+
+  // auth
   auth,
   googleProvider,
   onAuthStateChanged,
   signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
   signOut,
+
+  // firestore
   db,
   doc,
   getDoc,
@@ -80,11 +89,14 @@ export {
   orderBy,
   serverTimestamp,
 
-  // realtime db
+  // realtime database (RTDB)
   rtdb,
+  getDatabase,
   rtdbRef,
   rtdbGet,
   rtdbSet,
   rtdbUpdate,
   rtdbServerTimestamp,
+  rtdbOnValue,
+  rtdbPush,
 };
